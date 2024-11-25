619ac669af13a75c82bf89a6c32669aae4fc5982
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-25 10:20:25 +0100

    edit react library

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+export default function () {
+  return (
+    <>
+      <header>
+        <div className="logo">LOGO</div>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/Books">Books</a>
+          <a href="/about">About</a>
+          <a href="/contacts">Contacts</a>
+        </nav>
+      </header>
+      <main>
+        <h1>About</h1>
+      </main>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </>
+  );
+}
