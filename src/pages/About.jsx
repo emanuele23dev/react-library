1dfc4c5c61ff5cb404dfb20f92f1eff987d3f4f1
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-25 10:58:52 +0100

    edit react-library

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,11 @@
+import MainMenu from "../components/MainMenu";
+
 export default function () {
   return (
     <>
       <header>
         <div className="logo">LOGO</div>
-        <nav>
-          <a href="/">Home</a>
-          <a href="/Books">Books</a>
-          <a href="/about">About</a>
-          <a href="/contacts">Contacts</a>
-        </nav>
+        <MainMenu />
       </header>
       <main>
         <h1>About</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
+import MainMenu from "../components/MainMenu";
 export default function () {
   return (
     <>
       <header>
         <div className="logo">LOGO</div>
-        <nav>
-          <a href="/">Home</a>
-          <a href="/Books">Books</a>
-          <a href="/about">About</a>
-          <a href="/contacts">Contacts</a>
-        </nav>
+        <MainMenu />
       </header>
       <main>
         <h1>Home</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+          porro culpa sint iure quo odit provident rem facilis in dignissimos.
+        </p>
       </main>
 
       <footer>
